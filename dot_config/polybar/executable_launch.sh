a3e29809b923e17ad08478223be4af79e0de7d6c
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2020-12-28 01:17:47 -0500

    Update Debian configs

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+pkill polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1, bar2 and bar3
-polybar bar1 &
-polybar bar2 &
-polybar bar3
+# Launch bar
+polybar bar
 
 echo "Bars launched..."
